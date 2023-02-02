50031c1710c45a410f3076d85b7a86558274f758
Morgane Lejeune <morgane.lejeune@hotmail.fr>
2023-02-02 16:16:25 +0100

    #2 Feat/Recuperation-donnee-api: création d'une fonction de génération des projets triés, création du design de la page login.

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,101 +1,91 @@
+//
+
+const CATEGORIES = {
+  NONE : "Tous",
+  OBJECTS : "Objets",
+  APPARTMENTS : "Appartements",
+  HOTELS : "Hotels & restaurants"
+};
 
 
-// fonction asynchrone qui récupère les données
+// fonction de récupération des données sur le dom
+async function getProjects() {
+  const response = await fetch('http://localhost:5678/api/works')
+  .catch((error) => console.error(error));
+  const projects = await response.json()
+  return projects;
+}
 
-function getDataForDom() {
 
-  const projectsUrl = fetch('http://localhost:5678/api/works');
-  projectsUrl
-    .then((projectsUrl) => {
-      if(projectsUrl.ok){
-        return projectsUrl.json();
-      } else {
-        console.error("Erreur réponse :" + projectsUrl.status);
+
+// fonction de génération des projets triés
+async function projectsGenerator(){
+
+  //On attend la réponse, le resolve de la promesse de fetch.
+  const projects = await getProjects();
+  // Object.values permet de renvoyer un tableau
+  // contenant les valeurs de l'objet passé en param
+  const labels = Object.values(CATEGORIES);
+
+  const portfolio = document.getElementById("portfolio");
+  const myGallery = document.querySelector(".gallery");
+  const titlePortfolio = document.querySelector("#portfolio h2");
+  portfolio.prepend(titlePortfolio);
+  const filtersElement = document.createElement("div");
+  filtersElement.classList.add("filters_bloc");
+  portfolio.appendChild(filtersElement);
+  portfolio.insertBefore(filtersElement, myGallery);
+
+
+  displayProjects(projects)
+
+
+
+/* fonction qui prend par défaut aucune catégorie.
+Sinon on filtre dans un tableau, avec .filter()  */
+const getFilteredProjects = (category) => {
+  return () => {
+    // let est utilisé pour être réassigné afin qu'on puisse changer la catégorie
+    let filteredProjects;
+    if (category === CATEGORIES.NONE) {
+      filteredProjects = projects;
+    } else {
+        /* on appelle une méthode filter, et une fonction callback qui va comparer le nom sur l'api et celui de l'objet créee */
+        filteredProjects = projects.filter(projet => projet.category.name === category);
       }
-    })
-    .then(jsonListData => {
-      projectsGenerator(jsonListData);
-    })
-    .catch((error) => console.error(error));
+      // Dans tous les cas, on rafraichit la galerie on effaçant ce qui était avant
+      myGallery.innerHTML = "";
+      // Et on inclut la génération visuelle à au filtre choisi.
+      displayProjects(filteredProjects);
+    };
+  };
+
+  /*
+  Pour toutes les catégories, on crée l'ensemble des éléments, on lui assigne
+  sa catégorie
+  */
+  for (const label of labels) {
+    const btnFilter = document.createElement("button");
+    btnFilter.textContent = label;
+    filtersElement.appendChild(btnFilter);
+    btnFilter.addEventListener("click", getFilteredProjects(label));
   }
 
-  getDataForDom();
 
-
-  function projectsGenerator(jsonListData){
-
-    const myGallery = document.querySelector(".gallery");
-
-    for (let i = 0; i < jsonListData.length; i++) {
-
-    const jsonListArticle = jsonListData[i];
-
-    // figure qui recevront les balises img et figcaption
-    const projectElement = document.createElement("figure");
-
-    //création des balises et ajout des données
-
-    const imageElement = document.createElement("img");
-    imageElement.src = jsonListArticle.imageUrl;
-    imageElement.crossOrigin = "anonymous";
-    const figcaptionElement = document.createElement("figcaption");
-    figcaptionElement.innerText = jsonListArticle.title;
-
-    // rattachement des balises au Dom
-    myGallery.appendChild(projectElement);
-    projectElement.appendChild(imageElement);
-    projectElement.appendChild(figcaptionElement);
-        console.log(imageElement);
-      }
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-/*   async function getDataForDom() {
-    const response = await fetch('http://localhost:5678/api/works');
-    const jsonListData = await response.json();
-    return jsonListData;
+  // création des éléments des projets
+  function displayProjects(projects) {
+    for (const project of projects) {
+      const projectElement = document.createElement("figure");
+      const imageElement = document.createElement("img");
+      imageElement.src = project.imageUrl;
+      imageElement.crossOrigin = "anonymous";
+      const figcaptionElement = document.createElement("figcaption");
+      figcaptionElement.innerText = project.title;
+      myGallery.appendChild(projectElement);
+      projectElement.appendChild(imageElement);
+      projectElement.appendChild(figcaptionElement);
+    }
   }
+}
 
-
-
-     // Fonction de génération de projets
-
-function projectsGenerator(jsonListData){
-
-    const myGallery = document.querySelector(".gallery");
-
-    for (let i = 0; i < jsonListData.length; i++) {
-
-    const jsonListArticle = jsonListData[i];
-
-    // figure qui recevront les balises img et figcaption
-    const projectElement = document.createElement("figure");
-
-    //création des balises et ajout des données
-
-    const imageElement = document.createElement("img");
-    imageElement.src = jsonListArticle.imageUrl;
-    imageElement.crossOrigin = "anonymous";
-    const figcaptionElement = document.createElement("figcaption");
-    figcaptionElement.innerText = jsonListArticle.title;
-
-    // rattachement des balises au Dom
-    myGallery.appendChild(projectElement);
-    projectElement.appendChild(imageElement);
-    projectElement.appendChild(figcaptionElement);
-        console.log(imageElement);
-      }
-    };
- */
-
-
+projectsGenerator();
