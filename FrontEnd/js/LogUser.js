77d92db1d5945bbfff923c8be2fd7b8949e9f8fd
Morgane Lejeune <morgane.lejeune@hotmail.fr>
2023-02-20 10:33:42 +0100

    #5 Feat/Integration-page-connexion: Authentification de l'utilisateur, mise en cache du token.

--- FrontEnd/js/LogUser.js
+++ FrontEnd/js/LogUser.js
@@ -3,20 +3,44 @@ const mail = document.getElementById('email').value;
 const password = document.getElementById('password').value;
 
 
-form.addEventListener('submit', (e) => {
-e.preventDefault(); // empêche le formulaire de se soumettre normalement
-let formData = new FormData(form);
+const hasValidateToken = (data) => data?.userId ? true : false;
 
-fetch('http://localhost:5678/api/users/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(Object.fromEntries(formData)) // convertit FormData en objet JSON
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+const navigator = (data) => {
+  const windowLocation = window.location;
+  const canNavigate = hasValidateToken(data);
+  if (canNavigate) {
+    localStorage.setItem("token", data.token)
+    windowLocation.assign("http://127.0.0.1:5500/FrontEnd/index.html")
+  } else {
+    window.alert("Veuillez vérifier vos identifiants")
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // empêche le formulaire de se soumettre normalement
+  let formData = new FormData(form);
+
+  fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(Object.fromEntries(formData)) // convertit FormData en objet JSON
+    })
+  .then(response => response.json())
+  .then(data => navigator(data))
+  .catch(error => console.error(error));
 });
 
 
+
+
+/* then(response => response.json(), {
+  windowLocation.assign("http://127.0.0.1:5500/FrontEnd/index.html"),
+}
+) */
+
+/*
+
+
+} */
